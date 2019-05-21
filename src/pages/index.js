97744153eb9a4118a822be117d4fa72c7f82f708
Daniel Ryan <d@nielryan.com>
2019-05-21 12:48:16 -0600

    changed timing for index page animation

--- src/pages/index.js
+++ src/pages/index.js
@@ -19,7 +19,7 @@ export default class Index extends Component {
     FontAwesomeIcons.init();
 
     await this.cursorLine1();
-    await this.sleep(3500);
+    await this.sleep(3000);
     await this.setState({ cursorDisplay1: 'off' });
     await this.cursorLine2();
     await this.sleep(3000);
