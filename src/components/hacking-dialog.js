2a974610c6c9c62327f45a316a9c703446e59ca8
Daniel Ryan <d@nielryan.com>
2020-03-14 12:44:27 -0600

    Added "warning" to hacking dialog

--- src/components/hacking-dialog.js
+++ src/components/hacking-dialog.js
@@ -4,6 +4,7 @@ import styles from '../styles/hacking-dialog.module.css';
 export default function HackingDialog() {
   return (
     <div className={[styles.dialog, styles.flash].join(' ')}>
+      <h3 className={styles.dialogTitle}>WARNING</h3>
       Hacking in progress...
     </div>
   );
