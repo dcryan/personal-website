b8f3d4cbf464294d8b5bb8d9397a639a3751687c
Daniel Ryan <d@nielryan.com>
2020-03-14 12:45:02 -0600

    Added blog post element styling.

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -2,10 +2,9 @@ import React from 'react';
 import { Link, graphql } from 'gatsby';
 import PropTypes from 'prop-types';
 import Layout from '../components/layout';
-import Header from '../components/header';
 import FontAwesomeIcons from '../font-awesome';
 import SEO from '../components/seo';
-import Bio from '../components/bio';
+import styles from '../styles/blog-post.module.css';
 
 export default function Template({
   data, // this prop will be injected by the GraphQL query below.
@@ -15,44 +14,43 @@ export default function Template({
   const { markdownRemark: post } = data; // data.markdownRemark holds our post data
   const { previous, next } = pageContext;
   return (
-    <Layout>
-      <SEO
-        title={post.frontmatter.title}
-        description={post.frontmatter.description || post.excerpt}
-      />
-      <div dangerouslySetInnerHTML={{ __html: post.html }} />
-      <hr
-        style={{
-          marginBottom: 8,
-        }}
-      />
-      <Bio />
+    <>
+      <Layout>
+        <SEO
+          title={post.frontmatter.title}
+          description={post.frontmatter.description || post.excerpt}
+        />
+        <div
+          className={styles.content}
+          dangerouslySetInnerHTML={{ __html: post.html }}
+        />
 
-      <ul
-        style={{
-          display: `flex`,
-          flexWrap: `wrap`,
-          justifyContent: `space-between`,
-          listStyle: `none`,
-          padding: 0,
-        }}
-      >
-        <li>
-          {previous && (
-            <Link to={previous.fields.slug} rel="prev">
-              ← {previous.frontmatter.title}
-            </Link>
-          )}
-        </li>
-        <li>
-          {next && (
-            <Link to={next.fields.slug} rel="next">
-              {next.frontmatter.title} →
-            </Link>
-          )}
-        </li>
-      </ul>
-    </Layout>
+        <ul
+          style={{
+            display: `flex`,
+            flexWrap: `wrap`,
+            justifyContent: `space-between`,
+            listStyle: `none`,
+            padding: 0,
+          }}
+        >
+          <li>
+            {previous && (
+              <Link to={previous.fields.slug} rel="prev">
+                ← {previous.frontmatter.title}
+              </Link>
+            )}
+          </li>
+          <li>
+            {next && (
+              <Link to={next.fields.slug} rel="next">
+                {next.frontmatter.title} →
+              </Link>
+            )}
+          </li>
+        </ul>
+      </Layout>
+    </>
   );
 }
 
