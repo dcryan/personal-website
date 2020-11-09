5bc1feb7269cc8c21f9424fe7882e00a618d9147
Daniel Ryan <d@nielryan.com>
2020-11-09 09:51:13 +0100

    Add google analytics to head

--- gatsby-config.js
+++ gatsby-config.js
@@ -33,6 +33,7 @@ module.exports = {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
         trackingId: process.env.GOOGLE_ANALYTICS_TRACKING_ID || 'none',
+        head: true,
       },
     },
   ],
