869f6aa59a9d105616f5ee4f34184e5477b4fa2e
Daniel Ryan <d@nielryan.com>
2020-11-09 10:41:31 +0100

    GTag

--- gatsby-config.js
+++ gatsby-config.js
@@ -14,27 +14,9 @@ module.exports = {
     {
       resolve: `gatsby-plugin-google-gtag`,
       options: {
-        // You can add multiple tracking ids and a pageview event will be fired for all of them.
-        trackingIds: [
-          process.env.GOOGLE_ANALYTICS_TRACKING_ID, // Google Analytics / GA
-          // "AW-CONVERSION_ID", // Google Ads / Adwords / AW
-          // "DC-FLOODIGHT_ID", // Marketing Platform advertising products (Display & Video 360, Search Ads 360, and Campaign Manager)
-        ],
-        // This object gets passed directly to the gtag config command
-        // This config will be shared accross all trackingIds
-        gtagConfig: {
-          // optimize_id: 'OPT_CONTAINER_ID',
-          anonymize_ip: true,
-          cookie_expires: 0,
-        },
-        // This object is used for configuration specific to this plugin
+        trackingIds: [process.env.GOOGLE_ANALYTICS_TRACKING_ID],
         pluginConfig: {
-          // Puts tracking script in the head instead of the body
           head: true,
-          // Setting this parameter is also optional
-          respectDNT: true,
-          // Avoids sending pageview hits from custom paths
-          // exclude: ["/preview/**", "/do-not-track/me/too/"],
         },
       },
     },
